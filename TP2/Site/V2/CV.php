ab63e484f2e4b478f3675d6597015c2a69ec2dc6
RodoIanbou <97095045+RodoIanbou@users.noreply.github.com>
2023-03-14 17:59:31 +0100

    commit factorisation menu

--- TP2/Site/V2/CV.php
+++ TP2/Site/V2/CV.php
@@ -4,23 +4,7 @@
     
     <body id="page-top">
         <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
-            <div class="container">
-                <a class="navbar-brand" href="#page-top">Rodolphe IANBOUKHTINE</a>
-                <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="index.php">Accueil</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="projets.php">Projets</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a id="currentpage" class="nav-link py-3 px-0 px-lg-3 rounded"  href="CV.php">CV</a></li>
-                        
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <?php require_once('template_menu.php');?>
         <!-- Masthead-->
         <header class="masthead bg-primary text-white text-center">
             <div class="container d-flex align-items-center flex-column">
