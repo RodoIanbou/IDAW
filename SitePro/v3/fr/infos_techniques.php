940c6d717c429fbdbcae6642b1dbda426b950560
RodoIanbou <97095045+RodoIanbou@users.noreply.github.com>
2023-03-15 21:56:54 +0100

    site v3 terminé

---
+++ SitePro/v3/fr/infos_techniques.php
@@ -0,0 +1,50 @@
+<body id="page-top">
+        <!-- Masthead-->
+<header class="masthead bg-primary text-white text-center">
+    <div class="container d-flex align-items-center flex-column">
+        <!-- Masthead Avatar Image-->
+
+        <!-- Masthead Heading-->
+        <h1 class="masthead-heading text-uppercase mb-0">A propos du site</h1>
+        <!-- Icon Divider-->
+        <div class="divider-custom divider-light">
+            <div class="divider-custom-line"></div>
+
+
+        </div>
+        <!-- Masthead Subheading-->
+        <ul style="text-align: justify;">
+            <li><p class="masthead-subheading font-weight-light mb-0">Bienvenue sur notre page consacrée à la partie technique de notre site internet !</p></li>
+            <br>
+            <li><p class="masthead-subheading font-weight-light mb-0">Notre site a été développé en utilisant les langages HTML, CSS, PHP et JavaScript.</p></li>
+            <br>
+            <li><p class="masthead-subheading font-weight-light mb-0">Le HTML (HyperText Markup Language) est le langage de balisage utilisé pour créer la structure et le contenu de notre site web. Il nous permet de définir les différents éléments de notre page, tels que les en-têtes, les paragraphes, les images, les liens, etc.</p></li>
+            <br>
+            <li><p class="masthead-subheading font-weight-light mb-0">Le CSS (Cascading Style Sheets) est utilisé pour ajouter de la présentation et de la mise en forme à notre site web. Il nous permet de définir les couleurs, les polices, les marges, les tailles, les dispositions et d'autres styles visuels pour nos éléments HTML.</p></li>
+            <br>
+            <li><p class="masthead-subheading font-weight-light mb-0">Le PHP (Hypertext Preprocessor) est un langage de programmation utilisé côté serveur. Il nous permet de générer des pages web dynamiques en utilisant des scripts PHP pour interagir avec la base de données, pour récupérer des données stockées sur le serveur, pour afficher du contenu personnalisé, pour gérer des formulaires et plus encore.</p></li>
+            <br>
+            <li><p class="masthead-subheading font-weight-light mb-0">Enfin, le JavaScript est un langage de programmation côté client qui nous permet d'ajouter des fonctionnalités interactives à notre site web, telles que des menus déroulants, des diaporamas, des animations, des formulaires interactifs, etc.</p></li>
+            <br>
+            <li><p class="masthead-subheading font-weight-light mb-0">Grâce à ces quatre langages, notre site web offre une expérience utilisateur fluide et agréable. Je suis fier de mon travail acharné et de mon dévouement pour offrir un site web de qualité aux visiteurs.</p></li>
+        </ul>
+    </div>
+</header>
+
+
+
+<!-- Bootstrap core JS-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Core theme JS-->
+<script src="js/scripts.js"></script>
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+<!-- * *                               SB Forms JS                               * *-->
+<!-- * * Activate your form at https
+
+        < * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
